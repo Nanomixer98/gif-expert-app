09de8310c13baec586b3faecfa48477b263b6c28
Nanomixer98 <nasabmx@hotmail.com>
2022-01-27 20:18:08 -0600

    Created AddCategory component

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { AddCategory } from './components/AddCategory';
 
 export const GifExpertApp = () => {
+
+    // const categories = ['Pokemon', 'Half Life', 'Halo'];
+
+    const [categories, setCategories] = useState(['Pokemon', 'Half Life', 'Halo']);
+
+    // const handleAdd = () => {
+    //     // setCategories([...categories, 'COD']);
+    //     setCategories(cats => [...cats, 'COD']);
+    // }
+
+
+
     return (
     <>
         <h2>GifExpertApp</h2>
-        <hr/> 
+        <AddCategory setCategories={ setCategories }/>
+        <hr/>
+
+        {/* <button onClick={handleAdd}>Agregar</button> */}
+        <ol>
+            {
+                categories.map((category, i) => {
+                    return <li key={category}>{category}</li>
+                })
+            }
+        </ol>
     </>);
   }
