325d41c39f81aa08121ed30b61bbc32a7c2e0ac5
Nanomixer98 <nasabmx@hotmail.com>
2022-03-06 10:37:58 -0600

    Fixed reference of useEffect

--- src/hooks/useFetchGifs.js
+++ src/hooks/useFetchGifs.js
@@ -1,5 +1,4 @@
-import { useState } from "react"
-import { useEffect } from "react/cjs/react.development";
+import { useState, useEffect } from "react"
 import { getGifs } from "../helpers/getGifs";
 
 export const useFetchGifs = ( category ) => {
